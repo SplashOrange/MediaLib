16d8e369dd9d7ef324d3aa4c05279228a2979e9a
Hannah Johnson <hannah135johnson@yahoo.com>
2018-09-14 16:08:49 -0500

    1.1.2 step 17 complete

--- MediaLib.java
+++ MediaLib.java
@@ -10,5 +10,7 @@ public class MediaLib
    public static void main()
     {
         System.out.println("Welcome to your Media Library");
+        Song song1 = new Song();
+        System.out.println(song1);
     }
 }

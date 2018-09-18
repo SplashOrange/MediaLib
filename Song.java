c336825b0aadfe72e3e025d3ef3dfec7368a77e2
Hannah Johnson <hannah135johnson@yahoo.com>
2018-09-18 12:47:27 -0500

    Deleted unecessary things

--- Song.java
+++ Song.java
@@ -18,7 +18,7 @@ public class Song
         // initialise instance variables
         x = 0;
         title = "";
-        System.out.println("This is my song");
-        System.out.println("Isn't this exciting");
+        }
+        
     }
-}
+

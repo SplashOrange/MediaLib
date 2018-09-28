efff4be44b3134fd8b2696e30781069c9505b44d
Hannah Johnson <hannah135johnson@yahoo.com>
2018-09-28 13:01:49 -0500

    did stuff

--- MediaLib.java
+++ MediaLib.java
@@ -9,11 +9,14 @@ public class MediaLib
 {
    public static void main()
     {
-        System.out.println("Welcome to your Media Library");
+        System.out.println("Welcome to your Media Library!");
+        System.out.println(" ");
+        System.out.println("Your Music:");
         Song song1 = new Song();
-        song1.setTitle("Sweetener");  
+        song1.setTitle("    Sweetener");  
         System.out.println(song1.getTitle());
         song1.setRating(10);  
-        System.out.println(song1.getRating());
+        System.out.println("    Your rating for this song:");
+        System.out.println(     song1.getRating());
     }
 }

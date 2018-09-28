498ee27d401773326de17c1b7893aa8f74e7e97e
Hannah Johnson <hannah135johnson@yahoo.com>
2018-09-28 16:22:23 -0500

    Adding items

--- MediaLib.java
+++ MediaLib.java
@@ -10,13 +10,64 @@ public class MediaLib
    public static void main()
     {
         System.out.println("Welcome to your Media Library!");
+        
+        
         System.out.println(" ");
+        System.out.println(" ");
+        
+        
         System.out.println("Your Music:");
+        System.out.println(" ");
         Song song1 = new Song();
-        song1.setTitle("    Sweetener");  
+        song1.setTitle("   R.E.M.");  
         System.out.println(song1.getTitle());
+        song1.setArtist("   Artist: Ariana Grande");
+        System.out.println(song1.getArtist());
+        song1.setAlbum("    Album: Sweetener");
+        System.out.println(song1.getAlbum());
         song1.setRating(10);  
-        System.out.println("    Your rating for this song:");
-        System.out.println(     song1.getRating());
+        System.out.println("        Your rating: "+song1.getRating());
+        
+        System.out.println("");
+        
+        Song song2 = new Song();
+        song2.setTitle("   Sorry Not Sorry");
+        System.out.println(song2.getTitle());
+        song2.setArtist("   Artist: Demi Lovato");
+        System.out.println(song2.getArtist());
+        song2.setAlbum("    Album: Tell Me You Love Me");
+        System.out.println(song2.getAlbum());
+        song2.setRating(8);
+        System.out.println("        Your rating: "+song2.getRating());
+        
+        
+        System.out.println(" ");
+        System.out.println(" ");
+        
+        
+        System.out.println("Your Movies:");
+        System.out.println(" ");
+        Movie movie1 = new Movie();
+        movie1.setTitle("   Contagion");
+        System.out.println(movie1.getTitle());
+        movie1.setRating(10);
+        System.out.println("        Your rating: "+(movie1.getRating()));
+        
+        System.out.println(" ");
+        
+        Movie movie2 = new Movie();
+        
+        System.out.println(" ");
+        System.out.println(" ");
+        
+        
+        System.out.println("Your Books:");
+        System.out.println(" ");
+        Book book1 = new Book();
+        book1.setTitle("    The Andromeda Strain");
+        System.out.println(book1.getTitle());
+        book1.setRating(10);
+        System.out.println("        Your rating: "+(book1.getRating()));
+        
     }
 }
--- Song.java
+++ Song.java
@@ -10,6 +10,8 @@ public class Song
     // instance variables - replace the example below with your own
     private int rating;
     private String title;
+    private String artist;
+    private String album;
     /**
      * Constructor for objects of class Song
      */
@@ -18,17 +20,31 @@ public class Song
         // initialise instance variables
         rating = 0;
         title = "";
+        artist = "";
+        album = "";
         }
-    public String getTitle() {
-        return title;
-    }
     public void setTitle(String t) {
         title = t;
+    }
+        public String getTitle() {
+        return title;
     }
     public void setRating(int r) {
         rating = r;
     }   
-    public int getRating() {
+        public int getRating() {
         return rating;
+    }
+    public void setArtist(String a) {
+        artist = a;
+    }
+        public String getArtist() {
+        return artist;
+    }
+    public void setAlbum(String al) {
+        album = al;
+    }
+    public String getAlbum() {
+        return album;
     }
 }
